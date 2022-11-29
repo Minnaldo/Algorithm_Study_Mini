cff1c589d8662ab5792ac32a2d3a13b9e8fb90a7
minnaldo <minnaldo6602@gmail.com>
2022-11-29 20:00:19 +0900

    [+] add Lv2/nextbignumber

---
+++ Programmers/src/Lv2/colrowmul.java
@@ -0,0 +1,18 @@
+package Lv2;
+
+import java.io.*;
+
+public class colrowmul {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter((new OutputStreamWriter(System.out)));
+
+        int[][] arr1 = {{1,4}, {3,2}, {4,1}};
+        int[][] arr2 = {{3,3}, {3,3}};
+
+
+
+        int answer = 0;
+
+    }
+}
--- Programmers/src/Lv2/nextbignumber.java
+++ Programmers/src/Lv2/nextbignumber.java
@@ -7,9 +7,39 @@ public class nextbignumber {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter((new OutputStreamWriter(System.out)));
 
-        int n = 100000;
+        int n = 15;
 
         int answer = 0;
 
+        int one_count = 0;
+        String str = Integer.toBinaryString(n);
+        for(int i = 0; i < str.length(); i++)
+        {
+            char ch = str.charAt(i);
+            if(ch == '1')
+                one_count++;
+        }
+
+        int n_clone = n;
+        while(true)
+        {
+            int result = 0;
+            n_clone++;
+
+            String str_2 = Integer.toBinaryString(n_clone);
+            for(int i = 0; i < str_2.length(); i++)
+            {
+                char ch_2 = str_2.charAt(i);
+                if(ch_2 == '1')
+                    result++;
+            }
+
+            if(result == one_count) {
+                answer = n_clone;
+                break;
+            }
+        }
+
+        System.out.println(answer);
     }
 }
